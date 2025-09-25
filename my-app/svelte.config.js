b2096d652fbc0a74c45ab48ddcb1c251cca2f2ff
Henrik Bygren <henrik.bygren@gmail.com>
2025-09-25 16:43:17 +0200

    upload app

---
+++ my-app/svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		output: {
+			bundleStrategy: 'inline'
+		},
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/bygren.github.io' : ''
+		}
+	}
+};
+
+export default config;
